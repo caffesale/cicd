58a566b2067674477f05b0bd7fb6c2b46061f3e5
pglibrary80 <pglibrary80@gmail.com>
2024-12-04 19:52:13 +0900

    minus-function

---
+++ src/minus.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "@jest/globals";
+import { minus } from "./minus";
+
+describe("sum module", () => {
+  test("1 + 2 to equal 3", () => {
+    expect(minus(3, 2)).toBe(1);
+    expect(minus(1, 2)).toBe(-1);
+  });
+});
---
+++ src/minus.ts
@@ -0,0 +1,5 @@
+function minus(a: number, b: number) {
+  return a - b;
+}
+
+export { minus };
